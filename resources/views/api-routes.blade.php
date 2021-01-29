9b03cd7a7922a04ee95941062fc1cf2aaa5bd959
Sam Arossi Maosa <smaosa@strathmore.edu>
2021-01-29 14:58:35 +0300

    Added api name prefix inside generated api routes

--- resources/views/api-routes.blade.php
+++ resources/views/api-routes.blade.php
@@ -1,4 +1,4 @@
 /* Auto-generated {{$modelRouteName}} api routes */
-Route::group(["middleware"=>['auth:sanctum', 'verified']], function () {
+Route::group(["middleware"=>['auth:sanctum', 'verified'],'as' => 'api.'], function () {
     Route::apiResource('/{{$modelRouteName}}', \App\Http\Controllers\API\{{$controllerClassName}}::class)->parameters(["{{$modelRouteName}}" => "{{$modelVariableName}}"]);
 });

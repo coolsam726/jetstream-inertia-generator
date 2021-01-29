e21e3fccc1c6ec8f59f7581449177eee7ed235d4
Sam Arossi Maosa <smaosa@strathmore.edu>
2021-01-29 13:51:30 +0300

    Bug fix in title generation in permissions migration

--- resources/views/permissions.blade.php
+++ resources/views/permissions.blade.php
@@ -5,7 +5,9 @@
 use Carbon\Carbon;
 use Illuminate\Config\Repository;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class {{ $className }} extends Migration
 {
@@ -73,6 +75,19 @@ class {{ $className }} extends Migration
             'model_has_roles' => 'model_has_roles',
             'role_has_permissions' => 'role_has_permissions',
         ]);
+        // Add Title columns in case they don't exist.
+        $roles = $tableNames['roles'];
+        $permissions = $tableNames['permissions'];
+        Schema::table($roles, function (Blueprint $table) use($roles) {
+            $hasTitle = Schema::hasColumn($roles,"title");
+            if (!$hasTitle) $table->string('title')->nullable();
+        });
+        Schema::table($permissions, function (Blueprint $table) use ($permissions) {
+            if (!Schema::hasColumn($permissions,"title")) {
+                $table->string('title')->nullable();
+            }
+        });
+        // End add title
 
         DB::transaction(function () use($tableNames) {
             foreach ($this->permissions as $permission) {
@@ -130,19 +145,6 @@ class {{ $className }} extends Migration
             'model_has_roles' => 'model_has_roles',
             'role_has_permissions' => 'role_has_permissions',
         ]);
-        // Add Title columns in case they don't exist.
-        $roles = $tableNames['roles'];
-        $permissions = $tableNames['permissions'];
-        Schema::table($roles, function (Blueprint $table) use($roles) {
-            $hasTitle = Schema::hasColumn($roles,"title");
-            if (!$hasTitle) $table->string('title')->nullable();
-        });
-        Schema::table($permissions, function (Blueprint $table) use ($permissions) {
-            if (!Schema::hasColumn($permissions,"title")) {
-                $table->string('title')->nullable();
-            }
-        });
-        // End add title
         DB::transaction(function () use ($tableNames){
             foreach ($this->permissions as $permission) {
                 $permissionItem = DB::table($tableNames['permissions'])->where([

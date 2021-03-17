0e43e36a0ee8878682790419d0b7115eacd971c5
Sam Maosa <maosa.sam@gmail.com>
2021-03-17 07:57:10 +0300

    Fixed the reactivity of the JigTextarea component

--- resources/js/JigComponents/JigTextarea.vue
+++ resources/js/JigComponents/JigTextarea.vue
@@ -1,19 +1,23 @@
 <template>
-    <textarea class="border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" v-model="value" ref="txt"></textarea>
+    <textarea :value="modelValue" @input="onInput" class="border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" ref="txt"></textarea>
 </template>
 
 <script>
 export default {
     name: "JigTextarea",
-    props: ['value'],
-    emits: ['input'],
+    props: ['modelValue'],
+    emits: ['update:modelValue'],
     model: {
-        prop: 'value',
-        event: 'input'
+        prop: 'modelValue',
+        event: 'update:modelValue'
     },
     methods: {
         focus() {
             this.$refs.txt.focus()
+        },
+        onInput(e) {
+            console.log(e);
+            this.$emit('update:modelValue',e.target.value)
         }
     }
 }
